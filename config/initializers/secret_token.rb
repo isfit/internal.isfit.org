fc6700dffac71d071c08f0838e205c77731be1ae
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-07-03 23:15:20 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InternalIsfitOrg::Application.config.secret_token = 'cc706a7c4753a6bc5d18e8bf380a295ef737806f6a3820419173704aa806fd1b0c235c77dc0edcfb13009065553f2e357ba7a192dcea91377f9d8529b68604ae'
