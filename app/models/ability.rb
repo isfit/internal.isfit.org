86bbad96f8505bcd087b7719ad5b8860b773815c
Stian Fredrikstad <stian.fredrikstad@gmail.com>
2011-08-24 20:05:31 +0200

    na

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     if user.role?(:admin)
       can :manage, :all
     end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
