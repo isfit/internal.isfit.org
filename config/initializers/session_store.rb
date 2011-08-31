9910a86ee325b4d2dc1c2553ec32d1520ff790bf
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-08-31 23:31:12 +0200

    =>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-InternalIsfitOrg::Application.config.session_store :cookie_store, key: '_internal_isfit_org_session'
+InternalIsfitOrg::Application.config.session_store :cookie_store, :key => '_internal_isfit_org_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
