be249b28a8e1d89a9468aee1e45d431ee0277347
Dag-Inge Aas <daginge@gmail.com>
2011-11-03 10:58:40 +0100

    Fixed frontend articles, fuck yeah

--- app/assets/javascripts/frontend_articles.js
+++ app/assets/javascripts/frontend_articles.js
@@ -9,15 +9,15 @@ $('#frontend_fileupload').fileupload({
   dropzone: $("form.file_upload"),
   done: function (e, data) {
     str = "/system/original_pictures/"+data.result.photo.id+"/cropable/"+data.result.photo.original_picture_file_name
-    $("#img1").html("<img src="+str+" id='front_large' />");
-    $("#img2").html("<img src="+str+" id='front_small' />");
-    $("#img3").html("<img src="+str+" id='article' />");
+    $("#img1").html("<img src="+str+" id='frontend_front_large' />");
+    $("#img2").html("<img src="+str+" id='frontend_front_small' />");
+    $("#img3").html("<img src="+str+" id='frontend_article_img' />");
     $("#frontend_article_photo_id").val(data.result.photo.id);
-    addCropping(data.result.ratio);
+    frontendAddCropping(data.result.ratio);
   }
 });
-$("button").click(function() {
-  $("button").removeClass("primary");
+$("button.article").click(function() {
+  $("button.article").removeClass("primary");
   $(this).addClass("primary");
   $(".hidable").hide();
   $(".imgareaselect-outer").hide();
@@ -29,57 +29,57 @@ $("button").click(function() {
   $(".imgareaselect-handle").hide();
 
   if ($(this).attr("data-page") === "images"){
-  $(".imgareaselect-outer").show();
-  $(".imgareaselect-border1").show();
-  $(".imgareaselect-border2").show();
-  $(".imgareaselect-border3").show();
-  $(".imgareaselect-border4").show();
-  $(".imgareaselect-selection").hide();
-  $(".imgareaselect-handle").hide();
-}
+    $(".imgareaselect-outer").show();
+    $(".imgareaselect-border1").show();
+    $(".imgareaselect-border2").show();
+    $(".imgareaselect-border3").show();
+    $(".imgareaselect-border4").show();
+    $(".imgareaselect-selection").hide();
+    $(".imgareaselect-handle").hide();
+  }
 
-  
+
   $("#"+$(this).attr("data-page")).show();
 });
 $(".hidable").hide();
 $("#images").show();
 
-function addCropping(ratio) {
+function frontendAddCropping(ratio) {
 
-$('#front_large').imgAreaSelect({ 
-  minWidth: 346, 
-  minHeight: 154,
-  aspectRatio: '2.25:1',  
-  handles: true, 
-  onSelectEnd: function (img, selection) { 
-    $('#x1').val(Math.round(selection.x1*ratio)); 
-    $('#y1').val(Math.round(selection.y1*ratio)); 
-    $('#x2').val(Math.round(selection.x2*ratio)); 
-    $('#y2').val(Math.round(selection.y2*ratio)); 
+  $('#frontend_front_large').imgAreaSelect({ 
+    minWidth: 530, 
+    minHeight: 196,
+    aspectRatio: '2.70:1',  
+    handles: true, 
+    onSelectEnd: function (img, selection) { 
+      $('#x1').val(Math.round(selection.x1*ratio)); 
+      $('#y1').val(Math.round(selection.y1*ratio)); 
+      $('#x2').val(Math.round(selection.x2*ratio)); 
+      $('#y2').val(Math.round(selection.y2*ratio)); 
     } 
-});
-$('#front_small').imgAreaSelect({ 
-  minWidth: 220, 
-  minHeight: 131,
-  aspectRatio: '1.68:1',  
-  handles: true, 
-  onSelectEnd: function (img, selection) { 
-    $('#x1_1').val(Math.round(selection.x1*ratio)); 
-    $('#y1_1').val(Math.round(selection.y1*ratio)); 
-    $('#x2_1').val(Math.round(selection.x2*ratio)); 
-    $('#y2_1').val(Math.round(selection.y2*ratio)); 
+  });
+  $('#frontend_front_small').imgAreaSelect({ 
+    minWidth: 250, 
+    minHeight: 120,
+    aspectRatio: '2.08:1',  
+    handles: true, 
+    onSelectEnd: function (img, selection) { 
+      $('#x1_1').val(Math.round(selection.x1*ratio)); 
+      $('#y1_1').val(Math.round(selection.y1*ratio)); 
+      $('#x2_1').val(Math.round(selection.x2*ratio)); 
+      $('#y2_1').val(Math.round(selection.y2*ratio)); 
     } 
-});
-$('#article').imgAreaSelect({ 
-  minWidth: 447, 
-  minHeight: 346,
-  aspectRatio: '1.29:1',  
-  handles: true, 
-  onSelectEnd: function (img, selection) { 
-    $('#x1_2').val(Math.round(selection.x1*ratio)); 
-    $('#y1_2').val(Math.round(selection.y1*ratio)); 
-    $('#x2_2').val(Math.round(selection.x2*ratio)); 
-    $('#y2_2').val(Math.round(selection.y2*ratio)); 
+  });
+  $('#frontend_article_img').imgAreaSelect({ 
+    minWidth: 447, 
+    minHeight: 346,
+    aspectRatio: '1.29:1',  
+    handles: true, 
+    onSelectEnd: function (img, selection) { 
+      $('#x1_2').val(Math.round(selection.x1*ratio)); 
+      $('#y1_2').val(Math.round(selection.y1*ratio)); 
+      $('#x2_2').val(Math.round(selection.x2*ratio)); 
+      $('#y2_2').val(Math.round(selection.y2*ratio)); 
     } 
-});
+  });
 }
--- app/models/frontend_article.rb
+++ app/models/frontend_article.rb
@@ -4,8 +4,8 @@ class FrontendArticle < ActiveRecord::Base
   before_save :set_byline
 
   has_attached_file :frontend_article_image, styles:  {
-    front_large: {geometry: "346x154#", processors: [:cropper] },
-    front_small: {geometry: "220x131#", processors: [:cropper_half] },
+    front_large: {geometry: "530x196#", processors: [:cropper] },
+    front_small: {geometry: "250x120#", processors: [:cropper_half] },
     article: {geometry: "447x346#", processors: [:cropper_spp_one_third] }
   }
  
