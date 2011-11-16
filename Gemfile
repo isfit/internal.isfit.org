ad697c7aa98510f36b9ae2d7888151d7b48d8031
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-11-16 18:51:31 +0100

    Gemfile

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'memcache-client'
 gem 'therubyracer'
 gem 'mini_magick', :require=>'RMagick'
 gem 'rmagick', :require => false
+gem 'sdoc'
 
 #group :assets do
   gem 'sass-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # == User
+  #
+  # Hovedmodell for en bruker av systemet. Inneholder hjelpemetoder som kan
+  # brukes til å hente attributter, seksjoner, gjenger etc. av en bruker
+  
 
   validates :password, length: {minimum: 8}
   set_primary_key :id
@@ -9,14 +14,18 @@ class User < ActiveRecord::Base
 
   attr_readonly :username, :ldap_id, :email
 
+
+  #Return full name of user
   def full_name
     "#{given_name} #{family_name}"
   end
 
+  #Return full name reversed of user
   def full_name_reversed
     "#{family_name}, #{given_name}"
   end
 
+  #Return if user has role
   def role?(r)
     role = Role.where(name:r).first
     roles.include?(role)
