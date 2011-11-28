eab9eebc73d4972c6b6c1f763a275cd04ca95649
Dag-Inge Aas <daginge@gmail.com>
2011-11-28 21:30:48 +0100

    added editorial rights to spp_article

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,7 @@ class Ability
     end
     if user.role?(:editorial)
       can :manage, FrontendArticle
+      can :manage, SppArticle
       can :manage, StaticPage
     end
     if user.role?(:internal)
