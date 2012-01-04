65d7dda435dfdfce935546a247115d491ec9a47a
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-01-04 17:44:10 +0100

    Lagt til factories, og guard følger med

---
+++ spec/factories.rb
@@ -0,0 +1,26 @@
+FactoryGirl.define do
+  factory :user do |f|
+    f.sequence(:email) {|n| "foo#{n}@example.com"}
+    f.sequence(:username) {|n| "foo#{n}"}
+    f.password "secret123"
+    f.roles {|r| [r.association(:internal_role)]}
+  end
+
+  factory :admin_user, class: User do |f|
+    f.sequence(:email) {|n| "foo#{n}@example.com"}
+    f.sequence(:username) {|n| "foo#{n}"}
+    f.password "secret123"
+    f.roles {|r| [r.association(:internal_role), r.association(:admin_role)]}
+  end
+
+  factory :internal_role, class: Role do |f|
+    f.name "internal"
+  end
+  factory :admin_role, class: Role do |f|
+    f.name "admin"
+  end
+
+  factory :account do
+    name "TestAccount"
+  end
+end
