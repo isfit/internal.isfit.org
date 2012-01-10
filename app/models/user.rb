4fedcc5c12eb0564c8ede1b5602a82dbf8638b08
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-01-10 18:48:02 +0100

    Fikset den spørringen...

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ActiveRecord::Base
 
   attr_readonly :username, :ldap_id, :email
 
-
   #Return full name of user
   def full_name
     "#{given_name} #{family_name}"
@@ -27,9 +26,27 @@ class User < ActiveRecord::Base
 
   def self.in_festival(year=2013, sort_columns="family_name, given_name")
     id = Festival.find_by_year(year).id
-    users = User.includes(:positions => [:groups, {:groups => :section}]).where("sections.festival_id = #{id} OR groups.festival_id = #{id}").order(sort_columns)
-    return users
+    joins(:positions => :groups).joins("LEFT OUTER JOIN sections ON sections.id = groups.section_id").where("groups.festival_id = ? OR sections.festival_id = ?", id,id).group("users.id").order(sort_columns)
   end
+
+
+
+
+  #    id = Festival.find_by_year(year).id
+#    users = User.includes(:positions => [:groups, {:groups => :section}]).where("sections.festival_id = #{id} OR groups.festival_id = #{id}").order(sort_columns)
+#    return users
+#  end
+#  def self.in_festival(year=2013, sort_columns="family_name, given_name")
+#    id = Festival.find_by_year(year).id
+#    users = User.joins(:positions => :groups, :positions => [:groups => :section]).where("groups.festival_id = #{id} or sections.festival_id = #{id}").order(sort_columns).select("DISTINCT users.*")  
+#    # Jeg spanderer pizza på dne som klarer å fikse denne
+#    # User.joins(:positions => [:groups, {:groups => :festival}, {:groups => [:section, {:section => :festival}]}])
+#    full_users = []
+#    users.each do |u|
+#      full_users << User.find(u.id)
+#    end
+#    return full_users
+#  end
 
   def name_reversed
     "#{self.family_name}, #{self.given_name}"
