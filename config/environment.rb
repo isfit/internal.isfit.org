c6f10ee886fb44644b096f91e1a6172f99db1d2b
Stian Fredrikstad <stian@fredrikstad.de>
2012-03-01 20:35:39 +0100

    Hentet inn en liten linje fra gammel kode

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+
 # Initialize the rails application
 InternalIsfitOrg::Application.initialize!
