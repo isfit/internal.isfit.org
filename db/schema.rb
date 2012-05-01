49e140d7ad61ab4785b785bb86ce40dc6b285fcc
Sarah Ustvedt <sarahustvedt@dhcp-006100.wlan.ntnu.no>
2012-05-01 21:39:23 +0200

    Updated schema to new version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120430184250) do
+ActiveRecord::Schema.define(:version => 20120501181210) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
