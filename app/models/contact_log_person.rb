cd18f48a36f8fd555045bc8e730d8ab8a7ebb75b
Kristian Våge <krisvaage@gmail.com>
2012-05-01 22:56:50 +0200

    Models in contact log changed to belong to a status

--- app/models/contact_log_person.rb
+++ app/models/contact_log_person.rb
@@ -1,5 +1,6 @@
 class ContactLogPerson < ActiveRecord::Base
   belongs_to :contact_log_unit
+  belongs_to :contact_log_status
   has_many :contact_logs, :dependent => :delete_all
   # Return full name of user
   def full_name
--- app/models/contact_log_unit.rb
+++ app/models/contact_log_unit.rb
@@ -1,4 +1,5 @@
 class ContactLogUnit < ActiveRecord::Base
+  belongs_to :contact_log_status
   has_many :contact_log_people, :dependent => :destroy
   has_many :contact_logs, through: :contact_log_people
 end
