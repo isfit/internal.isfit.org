f483eaa3fe32fbd63aa948330670cef4b9ee0ce0
Christian Knappskog <ch_knapp@mac.com>
2012-05-02 17:10:06 +0200

    Cleaned out auto-generated test stubs

--- spec/models/spp_page_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe SppPage do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/who_am_i/highscore.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "who_am_i/highscore.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
