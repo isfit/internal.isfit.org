afa3170970651e04af230b28fe8b2743f3db6bc5
Kristian Våge <krisvaage@gmail.com>
2012-05-03 21:26:53 +0200

    Corrected indentation in previous migrations

--- db/migrate/20120430184702_create_contact_log_statuses.rb
+++ db/migrate/20120430184702_create_contact_log_statuses.rb
@@ -1,8 +1,8 @@
 class CreateContactLogStatuses < ActiveRecord::Migration
   def change
     create_table :contact_log_statuses do |t|
-	t.string :status
-	 t.string :color_code
+      t.string :status
+      t.string :color_code
       t.timestamps
     end
   end
