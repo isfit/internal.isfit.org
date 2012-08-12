1d2c48864aa5170dda6e70e7c4dea50038ec5b13
Erik Bergersen <bergersen.erik@gmail.com>
2012-08-12 14:38:52 +0200

    Fixed size for workshop image

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -4,8 +4,8 @@ class Workshop < ActiveRecord::Base
   attr_accessor :image_file_name, :image_content_type, :image_file_size, :image_updated_at
 
   has_attached_file :workshop_image,
-  	:styles => {
-  		:original => "250x530>",
-  		:thumb => "120x230>"
-  	}
+    :styles => {
+    :original => "940x480>",
+    :thumb => "120x230>"
+  }
 end
