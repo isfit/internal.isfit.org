9d43d5266faa5e23cd2ae6890785df4ae8509844
Dag-Inge Aas <daginge@gmail.com>
2012-08-17 19:13:15 +0200

    Add easter egg when logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+#!/bin/env ruby
+# encoding: utf-8
 class SessionsController < ApplicationController
 
   layout "login"
@@ -10,7 +12,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:login])
     if user && user.authenticate(params[:password]) && user.role?(:internal)
       session[:user_id] = user.id
-      redirect_to_target_or_default root_url, notice: "Logged in successfully."
+      redirect_to_target_or_default root_url, notice: generate_motivational(user)
     else
       flash.now[:alert] = "Invalid login or password."
       render action: 'new'
@@ -30,17 +32,17 @@ class SessionsController < ApplicationController
     new_pass = random_password
     user = User.find_by_username(params[:username])
     if user.nil?
-	flash.now[:alert] = "Could not find a user with username: " + params[:username]
-	return render :forgot_password
+      flash.now[:alert] = "Could not find a user with username: " + params[:username]
+      return render :forgot_password
     end
     user.password = new_pass
     if user.save!
-	user.changeLdapPassword(new_pass)
-    	UserMailer.forgot_password_mail(user.private_email, new_pass).deliver
-        return redirect_to login_path, :notice => "A new password has been sent to your private email"
+      user.changeLdapPassword(new_pass)
+      UserMailer.forgot_password_mail(user.private_email, new_pass).deliver
+      return redirect_to login_path, :notice => "A new password has been sent to your private email"
     else
-	flash.now[:alert] = "Could not generate new password please contact orakel@isfit.org"
-	return render :forgot_password	
+      flash.now[:alert] = "Could not generate new password please contact orakel@isfit.org"
+      return render :forgot_password	
     end
   end
 
@@ -48,6 +50,26 @@ class SessionsController < ApplicationController
   def random_password(size = 8) 
     chars = (('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 I l 0) 
     (1..size).collect{|a| chars[rand(chars.size)] }.join 
+  end
+
+  def generate_motivational(user)
+    motivationals = []
+    motivationals << "Hei #{user.full_name}! Lenge siden sist!"
+    motivationals << "Takk for at du er den du er #{user.given_name}."
+    motivationals << "Håper du får en fin dag!"
+    motivationals << "Du er et unika, #{user.given_name}"
+    motivationals << "Så fin du var i dag, #{user.given_name}!"
+    motivationals << "Om bare all var som deg, #{user.given_name}"
+    motivationals << "Tenk, uten deg, hadde det ikke blitt noe festival!"
+    motivationals << "Har du gitt IT en klem i det siste? Jeg har hørt at de liker klemmer."
+    motivationals << "Hva leter du etter i dag da, #{user.given_name}?"
+    motivationals << "Skal vi gifte oss, #{user.given_name}?"
+    motivationals << "Sliter du med motivasjonen? Se: [How bad do you want it?](http://www.youtube.com/watch?v=lsSC2vx7zFQ)"
+    motivationals << "Tenk at du, faktisk er du. Har du noen gang tenkt over hvor fantastisk du faktisk er?"
+    motivationals << "Om du bare vil det, så klarer du det. Du kan klare absolutt alt du vil, fordi du er enestående!"
+    motivationals << "Har du det bra i dag, #{user.given_name}?"
+    
+    return motivationals[rand(motivationals.size)]
   end
 
 end
