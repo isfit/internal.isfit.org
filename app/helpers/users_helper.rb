c0c2e2db403c456c6512e528c056a856785e2ab2
Erik Bergersen <bergersen.erik@gmail.com>
2012-08-20 20:04:55 +0200

    Added options to download csv-files of all users with cardnumber

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,18 @@
 module UsersHelper
+
+require 'csv'
+
+def csv_user_list
+  CSV.generate do |csv|
+        csv << ["Last name", "First name", "Username", "Cardnumber Samfundet"]
+    User.in_festival.all(:select => ["given_name", "family_name", "username", "cardnumber_samfundet"]).each do |user|
+      if user.cardnumber_samfundet.to_s.empty?
+        csv << [user.family_name, user.given_name, user.username]
+      else
+        csv << [user.family_name, user.given_name, user.username, user.cardnumber_samfundet]
+      end
+    end
+  end
+end
+
 end
