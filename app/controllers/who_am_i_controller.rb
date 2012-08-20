53fc2975359452cd1879edb3baed1c4c27203431
Matias Pettersen <matias1992@dhcp-015202.wlan.ntnu.no>
2012-08-20 22:00:29 +0200

    Add Game view

--- app/controllers/who_am_i_controller.rb
+++ app/controllers/who_am_i_controller.rb
@@ -3,12 +3,25 @@ class WhoAmIController < ApplicationController
   end
 
   def game
-  	@user1 = User.find(848)
-  	@user2 = User.find(131)
-    @user3 = User.find(123)
+    @users = User.random.where("facebook_id IS NOT NULL")
+    @user1 = @users[0]
+    @user2 = @users[1]
+    @user3 = @users[2]
+    @user4 = @users[3]
+    @random_number = Random.new.rand(4)
+    @correct_user = @users[@random_number]
+
+    @session = WhoAmI.new
+    @session.user1_id = @users[0].id
+    @session.user2_id = @users[1].id
+    @session.user3_id = @users[2].id
+    @session.user4_id = @users[3].id
+    @session.correct_user_id = @correct_user.id
+    @session.save
   end
 
   def feedback
+    @session.correct_user_id 
   end
 
   def highscore
