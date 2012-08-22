3ad240f2566acc0a7bd46bc103b9eb495318e649
Erik Bergersen <bergersen.erik@gmail.com>
2012-08-22 14:52:18 +0200

    Added ntnu cardnumber to user csv-file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,16 @@
-module UsersHelper
-
 require 'csv'
 
-def csv_user_list
-  CSV.generate(:col_sep => ";") do |csv|
-        csv << ["Last name", "First name", "Username", "Cardnumber Samfundet"]
-    User.in_festival.all(:select => ["given_name", "family_name", "username", "cardnumber_samfundet"]).each do |user|
-      if user.cardnumber_samfundet.to_s.empty?
-        csv << [user.family_name, user.given_name, user.username]
-      else
-        csv << [user.family_name, user.given_name, user.username, user.cardnumber_samfundet]
+module UsersHelper
+
+  def csv_user_list
+    CSV.generate(:col_sep => ";") do |csv|
+      csv << ["Last name", "First name", "Username", "Cardnumber Samfundet", "Cardnumber NTNU"]
+      User.in_festival.all(:select => ["given_name", "family_name", "username", "cardnumber_samfundet", "cardnumber_ntnu"]).each do |user|
+        csv << [user.family_name, user.given_name, user.username, 
+          user.cardnumber_samfundet.to_s.empty? ? "NO" : user.cardnumber_samfundet,
+          user.cardnumber_ntnu.to_s.empty? ? "NO" : user.cardnumber_ntnu]
       end
     end
   end
-end
 
 end
