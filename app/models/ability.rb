2b7ae2d87a3a07324c7e15a56bac130c0e87b9d9
Dag-Inge Aas <daginge@gmail.com>
2012-08-23 17:49:56 +0200

    Add rights for everyone to post article

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -33,7 +33,7 @@ class Ability
     if user.role?(:internal)
       can :manage, Account
       can :read, StaticPage
-      can :read, Article
+      can [:read,:create,:update], Article
       can :manage, Kvitter
       can :stats, Participant
       can [:read,:create], RoomBooking
