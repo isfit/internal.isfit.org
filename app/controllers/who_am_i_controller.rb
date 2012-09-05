4d8c6cc38f2f16e1a8339d948275d7248489b7bc
Matias Pettersen <matias1992@dhcp-013071.wlan.ntnu.no>
2012-09-05 18:53:27 +0200

    Who am i

--- app/controllers/who_am_i_controller.rb
+++ app/controllers/who_am_i_controller.rb
@@ -3,26 +3,33 @@ class WhoAmIController < ApplicationController
   end
 
   def game
-    @users = User.random(2013,4).where("facebook_id IS NOT NULL")
-    @correct_user = @users[Random.rand(@users.length)]
+    @session = WhoAmI.where(user_id: current_user.id).order("created_at DESC").limit(1).first
 
-    @session = WhoAmI.new
-    @session.correct_user_id = @correct_user.id
-    @session.user_id = current_user.id
-    @session.save
+    if @session==nil || @session.played
+      @users = User.random(2013,4).where("facebook_id IS NOT NULL")
+      @correct_user = @users[Random.rand(@users.length)]
+      @session = WhoAmI.new 
+      @session.correct_user_id = @correct_user.id
+      @session.user_id = current_user.id
+      @session.save
+    end
 
+    @correct_user = User.find(@session.correct_user_id)
+    
+    
     if request.post?
       @obj = WhoAmI.find(params[:session])
       answered_user_id = params[:user_id]
       if answered_user_id.to_i == @obj.correct_user_id.to_i
-        @obj.answer = true
+          @obj.answer = true
       else 
         @obj.answer = false
       end
-      @obj.played = true
-      @obj.save
+    @obj.played = true
+    @obj.save
     end
   end
+
 
   def feedback
     @session.correct_user_id 
@@ -32,11 +39,11 @@ class WhoAmIController < ApplicationController
     whoAmIs = WhoAmI.where(played: true)
     points = Hash.new {|h,k| h[k] = 0} #Which player has most correct guesses?
     played = Hash.new {|h,k| h[k] = 0} #Used to find best played based correct/wrong ratio
+    puts played
     best_ratio = Hash.new {|h,k| h[k] = 0} #Which player has the best correct/wrong ratio?
 
     whoAmIs.each do |who|
         played[User.find(who.user_id)] += 1
-
 
         if who.answer == true
           points[User.find(who.user_id)] += 1
