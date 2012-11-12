747d5d916ba10169f1a99d9dac59068b049b9d98
Dag-Inge Aas <daginge@gmail.com>
2012-11-13 00:42:15 +0100

    Fixed weird bug with jquery and sessions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery_ujs
 //= require jquery
+//= require jquery_ujs
 //= require jquery-ui
 //= require_tree ./jquery
 //= require private_pub
