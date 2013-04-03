ad77f5ea084090c7ee8a3d5d46de9e9faccead1e
Erik Bergersen <bergersen.erik@gmail.com>
2013-04-03 18:42:25 +0200

    Show all participants in participants#index

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -3,7 +3,7 @@ class ParticipantsController < ApplicationController
 
   def index
     @search = Participant.search(params[:q])
-    @participants = @search.result.where("country_id = 8 or country_id = 11 or country_id = 164 or country_id = 29")
+    @participants = @search.result
   end
 
   def show
