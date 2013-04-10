66d16f72e1379424564e5241d0e34688060898c4
Kristian Våge <krisvaage@gmail.com>
2013-04-10 13:09:59 +0200

    Extracted methods in what am i controller

--- app/controllers/what_am_i_controller.rb
+++ app/controllers/what_am_i_controller.rb
@@ -5,31 +5,36 @@ require_dependency 'am_i_games/what_am_i_scorer'
 require_dependency 'am_i_games/what_am_i_creator'
 
 class WhatAmIController < ApplicationController
-
   load_and_authorize_resource
 
   def game
-    if request.post?
-      scorer = Internal::AmIGames::WhatAmIScorer.new(params)
-      scorer.score
-      
-      if scorer.correct?
-        flash[:notice] = "Korrekt!"
-      else
-        correct_user = scorer.correct_user
-        flash[:alert] = "Feil desverre, #{correct_user.full_name}  " +
-                        "#{correct_user.positions.last.title_no} " +
-                        "var det riktige svaret. Prøv igjen da vel!"
-      end
-    end
-
-    creator = Internal::AmIGames::WhatAmICreator.new current_user
-    @game = creator.game
-    @users = creator.users
-    @correct_user = creator.correct_user
+    score_game(params) if request.post?
+    create_new_game
   end
 
   def highscore
     @highscorer = Internal::AmIGames::Highscorer.new(:what_am_is)
+  end
+
+  private
+  def score_game params
+    scorer = Internal::AmIGames::WhatAmIScorer.new(params)
+    scorer.score
+
+    if scorer.correct?
+      flash[:notice] = "Korrekt!"
+    else
+      correct_user = scorer.correct_user
+      flash[:alert] = "Feil desverre, #{correct_user.full_name}  " +
+                      "#{correct_user.positions.last.title_no} " +
+                      "var det riktige svaret. Prøv igjen da vel!"
+    end
+  end
+
+  def create_new_game
+    creator = Internal::AmIGames::WhatAmICreator.new current_user
+    @game = creator.game
+    @users = creator.users
+    @correct_user = creator.correct_user
   end
 end
