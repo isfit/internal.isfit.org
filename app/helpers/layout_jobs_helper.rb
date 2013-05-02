21d34af6107cef0c45b7d5975410e832bd6eeb35
Kristian Våge <krisvaage@gmail.com>
2013-05-02 10:52:37 +0200

    layout_jobs: Google calendar

--- app/helpers/layout_jobs_helper.rb
+++ app/helpers/layout_jobs_helper.rb
@@ -1,2 +1,9 @@
 module LayoutJobsHelper
+  def google_date(date_string)
+    if date_string.nil?
+      Time.now.to_s[0..9].gsub('-', '')
+    else
+      date_string.to_s.gsub('-', '')
+    end
+  end
 end
