d6e19e4809821272f3d74e213ba503c4c1e872bf
Kristian Våge <krisvaage@gmail.com>
2013-12-14 12:54:39 +0100

    Change handling of secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-InternalIsfitOrg::Application.config.secret_token = '6d962b578883a443f73c7102a70158f057bcf5d89a98e2b1c038eaa2cfd095c254058b966938c10865a79283757b0c8a9b0bf8144d6ddffce25486792dd18a25'
+InternalIsfitOrg::Application.config.secret_token = YAML::load(File.open("#{Rails.root}/config/secret.yml"))
