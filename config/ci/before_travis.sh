cc222dfb4e263aa08469680442c7b098daf6eccc
Kristian Våge <krisvaage@gmail.com>
2013-12-14 13:46:11 +0100

    Secret must be created before running other rake tasks

--- config/ci/before_travis.sh
+++ config/ci/before_travis.sh
@@ -3,10 +3,10 @@ cp config/database.yml.example config/database.yml
 
 mysql -e 'create database isfit13_internal_test;'
 
+# Add secret
+bundle exec rake secret > config/secret.yml
+
 # Set up database
 echo "Set up test database"
 bundle exec rake db:create
 bundle exec rake db:schema:load
-
-# Add secret
-bundle exec rake secret > config/secret.yml
