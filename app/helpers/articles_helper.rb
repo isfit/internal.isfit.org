89b9091d550f674a9d9faeb43c1f3dc383b976b6
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-02-24 20:22:45 +0100

    Moved ugly code from view to helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def current_user_can_see_applications?
+    if current_user.role?(:admin) || current_user.role?(:recruiting) || current_user.role?(:board) || current_user.role?(:wingman) || current_user.role?(:interviewer) || current_user.role?(:leader)
+      true
+    else
+      false
+    end
+  end
 end
