ef445a8cafdc1953f1e7ea28d76445a9e68f342c
annabom <selvaag.braadland@gmail.com>
2014-08-25 21:51:44 +0200

    Added new mails

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -2,14 +2,14 @@
 class SubscriberMailer < ActionMailer::Base
 	def article_mail(username, article)
 		subject = "[ISFiT] [New article] #{article.title} "
-    body = "Artikkelen #{article.title} er oppdatert. Gå til https://internal.isfit.org/article/#{article.id} for å se den nyeste artikkelen "
+    body = "Artikkelen #{article.title} er oppdatert. Gå til https://internal.isfit.org/article/#{article.id} for å se den nyeste artikkelen \n \n -- \n Hilsen ISFiT <3"
 
     mail(to: "#{username}@isfit.org", from: 'orakel@isfit.org', subject: subject, body: body)
 	end
 
 	def kvitter_mail(username, kvitter)
 	subject = "[ISFiT] [New kvitter] #{kvitter.user.username} "
-    body = "#{kvitter.user.username} har kvittret! Her er den fantastiske meldingen: #{kvitter.message}"
+    body = "#{kvitter.user.username} har kvittret! Dette er #{kvitter.user.username}s meningsfulle budskap: #{kvitter.message}. Gå til internal(https://internal.isfit.org) for å se de andre som har kvitret!!! \n \n -- \n Hilsen ISFiT <3"
     mail(to: "#{username}@isfit.org", from: 'orakel@isfit.org', subject: subject, body: body)
 	end
 end
