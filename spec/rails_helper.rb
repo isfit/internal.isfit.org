800d77feea9aaa7441c7916ced7d46d92247a615
Filip Egge <filip.egge@gmail.com>
2014-08-25 03:34:56 +0200

    Capybara webkit driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 require 'cancan/matchers'
-
+Capybara.javascript_driver = :webkit
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
