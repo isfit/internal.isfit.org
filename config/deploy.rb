b0c74d49895b4f2d0df059f35414d8314e651319
Filip Egge <filip.egge@gmail.com>
2014-09-10 20:37:41 +0200

    Adds staging to deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,58 @@
-# RVM bootstrap
-#$:.unshift(File.expand_path("~/.rvm/lib"))
-require 'rvm/capistrano'
-require "whenever/capistrano"
-set :rvm_ruby_string, '1.9.3-p392'
-set :rvm_type, :user
+# config valid only for Capistrano 3.1
+lock '3.2.1'
 
-require 'bundler/capistrano'
+set :application, 'isfit.org'
+set :repo_url, 'git@example.com:isfit/internal.isfit.org.git'
 
-set :application, "isfit.org"
-set :repository,  "https://github.com/isfit/internal.isfit.org.git"
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-set :scm, :git
-set :whenever_command, "bundle exec whenever"
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
 
-role :web, "nova.isfit.org"                          # Your HTTP server, Apache/etc
-role :app, "nova.isfit.org"                          # This may be the same as your `Web` server
-role :db,  "nova.isfit.org", :primary => true # This is where Rails migrations will run
+# Default value for :scm is :git
+# set :scm, :git
 
-set :branch, "master"
-set :user, "passenger"
-set :use_sudo, false
-set :deploy_to, "/srv/www/internal.isfit.org"
-set :deploy_via, :remote_cache
-default_run_options[:pty] = true  # Must be set for the password prompt from git to work
-ssh_options[:forward_agent] = true
+# Default value for :format is :pretty
+# set :format, :pretty
 
-`date >> config/last_deploy.txt`
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-  task :symlink_shared, :roles => :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/oauth.yml #{release_path}/config/oauth.yml"
-    run "ln -nfs #{shared_path}/config/password.yml #{release_path}/config/password.yml"
-    run "ln -nfs #{shared_path}/config/secret.yml #{release_path}/config/secret.yml"
-  end
-end
-before "whenever:update_crontab", "bundle:install"
-after 'deploy:update_code', 'deploy:symlink_shared'
 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
