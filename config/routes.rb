1a37d90e17110b93fe001e64967cbd4ed3ba8e9e
Filip Egge <filip.egge@gmail.com>
2014-10-27 18:41:21 +0100

    Change route to forgot password from get to post

--- config/routes.rb
+++ config/routes.rb
@@ -216,7 +216,7 @@ InternalIsfitOrg::Application.routes.draw do
   get 'logout' => 'sessions#destroy', :as => :logout
   get 'login' => 'sessions#new', :as => :login
   get 'forgot_password' => 'sessions#forgot_password', :as => :forgot_password
-  get 'mail_password' => 'sessions#mail_password', :as => :mail_password
+  post 'mail_password' => 'sessions#mail_password', :as => :mail_password
   resources :sessions
   resources :users do 
     collection do
